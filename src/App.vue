88dd3d6fa548122c38e042a38fce9bcbe7570de7
saniok333 <alex.o.csc@gmail.com>
2020-01-09 14:54:20 +0200

    add toolbar, components, adaptation, routs

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,50 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+    <v-navigation-drawer app temporary v-model="drawer">
+      <v-list>
+        <v-list-item v-for="link of links" :key="link.title" :to="link.url">
+          <v-list-item-icon>
+            <v-icon>{{link.icon}}</v-icon>
+          </v-list-item-icon>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+          <v-list-item-content>
+            <v-list-item-title v-text="link.title"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
 
+    <v-app-bar app color="primary" dark>
+      <v-app-bar-nav-icon @click="drawer = !drawer" class="hidden-md-and-up"></v-app-bar-nav-icon>
+      <v-toolbar-title>Ad application</v-toolbar-title>
       <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn text v-for="link in links" :key="link.title" :to="link.url">
+          <v-icon left>{{link.icon}}</v-icon>
+          {{link.title}}
+        </v-btn>
+      </v-toolbar-items>
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <router-view></router-view>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
+  data() {
+    return {
+      drawer: false,
+      links: [
+        { title: "Login", icon: "mdi-lock", url: "/login" },
+        { title: "Registration", icon: "mdi-face", url: "/registration" },
+        { title: "Orders", icon: "mdi-bookmark-outline", url: "/orders" },
+        { title: "New ad", icon: "mdi-file-plus", url: "/new" },
+        { title: "My ads", icon: "mdi-menu", url: "/list" }
+      ]
+    };
+  }
 };
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,51 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Router from 'vue-router'
+import Home from '@/components/Home'
+import Ad from '@/components/Ads/Ad'
+import AdList from '@/components/Ads/AdList'
+import NewAd from '@/components/Ads/NewAd'
+import Login from '@/components/Auth/Login'
+import Registration from '@/components/Auth/Registration'
+import Orders from '@/components/User/Orders'
 
-Vue.use(VueRouter)
+Vue.use(Router)
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
-
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+export default new Router({
+  routes: [{
+      path: '',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/ad/:id',
+      name: 'ad',
+      component: Ad
+    },
+    {
+      path: '/list',
+      name: 'list',
+      component: AdList
+    },
+    {
+      path: '/new',
+      name: 'newAd',
+      component: NewAd
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/registration',
+      name: 'reg',
+      component: Registration
+    },
+    {
+      path: '/orders',
+      name: 'orders',
+      component: Orders
+    }
+  ],
+  mode: 'history'
 })
-
-export default router
