e274c16c9a6f015e4e439266fe7df071f1a18f35
saniok333 <alex.o.csc@gmail.com>
2020-01-14 15:54:24 +0200

    OrdersPage pre-implementation

--- src/components/User/Orders.vue
+++ src/components/User/Orders.vue
@@ -1,8 +1,22 @@
 <template>
   <v-container>
     <v-layout row>
-      <v-flex xs12>
-        <h1>Orders</h1>
+      <v-flex xs12 sm6 offset-sm3>
+        <h1 class="text--secondary mb-3">Orders</h1>
+        <v-list subheader two-line flat>
+          <v-list-item v-for="order in orders" :key="order.id">
+            <v-list-item-action>
+              <v-checkbox color="success" :input-value="order.done" @click="markDone(order)"></v-checkbox>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>{{order.name}}</v-list-item-title>
+              <v-list-item-subtitle>{{order.phone}}</v-list-item-subtitle>
+            </v-list-item-content>
+            <v-list-item-action>
+              <v-btn :to="'/ad/' + order.adId" class="primary">Open</v-btn>
+            </v-list-item-action>
+          </v-list-item>
+        </v-list>
       </v-flex>
     </v-layout>
   </v-container>
@@ -11,7 +25,22 @@
 <script>
 export default {
   data() {
-    return {};
+    return {
+      orders: [
+        {
+          id: "jhg2",
+          name: "Alex",
+          phone: "8-067-658-34-52",
+          adId: "765",
+          done: false
+        }
+      ]
+    };
+  },
+  methods: {
+    markDone(order) {
+      order.done = true;
+    }
   }
 };
 </script>
