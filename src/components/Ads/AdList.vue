acd912a352467d5165c19c634cbb94a414577e95
saniok333 <alex.o.csc@gmail.com>
2020-01-15 13:05:13 +0200

    AdListPage pre-implementation

--- src/components/Ads/AdList.vue
+++ src/components/Ads/AdList.vue
@@ -1,8 +1,25 @@
 <template>
   <v-container>
     <v-layout row>
-      <v-flex xs12>
-        <h1>AdList</h1>
+      <v-flex xs12 sm6 offset-sm3>
+        <h1 class="text--secondary mb-3">My ads</h1>
+        <v-card class="elevation-10 mb-3" v-for="ad in ads" :key="ad.id">
+          <v-layout>
+            <v-flex xs4>
+              <v-img height="160px" :src="ad.imageSrc"></v-img>
+            </v-flex>
+            <v-flex xs8>
+              <v-card-text>
+                <h2 class="text--primary">{{ad.title}}</h2>
+                <p>{{ad.description}}</p>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn class="info" :to="'/ad/' + ad.id">Open</v-btn>
+              </v-card-actions>
+            </v-flex>
+          </v-layout>
+        </v-card>
       </v-flex>
     </v-layout>
   </v-container>
@@ -11,7 +28,24 @@
 <script>
 export default {
   data() {
-    return {};
+    return {
+      ads: [
+        {
+          title: "First ad",
+          description: "Hello i am description",
+          promo: false,
+          imageSrc: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg",
+          id: "123"
+        },
+        {
+          title: "Second ad",
+          description: "Hello i am description",
+          promo: true,
+          imageSrc: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
+          id: "1234"
+        }
+      ]
+    };
   }
 };
 </script>
