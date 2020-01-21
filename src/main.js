4526aa411887081d5d20b7aba66cb6fc326e3844
saniok333 <alex.o.csc@gmail.com>
2020-01-21 18:21:25 +0200

    firebase pre-implementation

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
+import * as fb from 'firebase'
 
 Vue.config.productionTip = false
 
@@ -10,5 +11,15 @@ new Vue({
   router,
   store,
   vuetify,
+  created() {
+    fb.initializeApp({
+      apiKey: 'AIzaSyD5mgcTCPSiTxPfmTmBmPFWZU7GdR9l1BQ',
+      authDomain: 'vue-project-132f7.firebaseapp.com',
+      databaseURL: 'https://vue-project-132f7.firebaseio.com',
+      projectId: 'vue-project-132f7',
+      storageBucket: 'vue-project-132f7.appspot.com',
+      messagingSenderId: '1075767797368'
+    })
+  },
   render: h => h(App)
 }).$mount('#app')
