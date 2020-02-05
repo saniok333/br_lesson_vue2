2b3ad29df2e852ddd32c5742f9a8797eb606e2b2
saniok333 <alex.o.csc@gmail.com>
2020-02-05 20:01:28 +0200

    Ad editing pre-implementation

--- src/components/Ads/Ad.vue
+++ src/components/Ads/Ad.vue
@@ -2,7 +2,7 @@
   <v-container>
     <v-layout row>
       <v-flex xs12>
-        <v-card>
+        <v-card v-if="!loading">
           <v-img height="300px" :src="ad.imageSrc"></v-img>
           <v-card-text>
             <h1 class="text--primary">{{ad.title}}</h1>
@@ -10,23 +10,34 @@
           </v-card-text>
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn class="warning" flat>Edit</v-btn>
+            <app-edit-ad-modal :ad="ad"></app-edit-ad-modal>
             <v-btn class="success">Buy</v-btn>
           </v-card-actions>
         </v-card>
+        <div v-else class="text-center pt-5">
+          <v-progress-circular :size="100" :width="4" color="purple" indeterminate></v-progress-circular>
+        </div>
       </v-flex>
     </v-layout>
   </v-container>
 </template>
 
 <script>
+import EditAdModal from "./EditAdModal";
+
 export default {
   props: ["id"],
   computed: {
     ad() {
       const id = this.id;
       return this.$store.getters.adById(id);
+    },
+    loading() {
+      return this.$store.getters.loading;
     }
+  },
+  components: {
+    appEditAdModal: EditAdModal
   }
 };
 </script>
---
+++ src/components/Ads/EditAdModal.vue
@@ -0,0 +1,74 @@
+<template>
+  <v-dialog width="400px" v-model="modal">
+    <template v-slot:activator="{ on }">
+      <v-btn text class="warning" v-on="on">Edit</v-btn>
+    </template>
+    <v-card>
+      <v-container>
+        <v-layout row>
+          <v-flex xs12>
+            <v-card-title>
+              <h1 class="text--primary">Edit ad</h1>
+            </v-card-title>
+          </v-flex>
+        </v-layout>
+        <v-divider></v-divider>
+        <v-layout row>
+          <v-flex xs12>
+            <v-card-text>
+              <v-text-field label="Title" name="title" type="text" v-model="editedTitle" />
+              <v-text-field
+                label="Description"
+                name="description"
+                type="text"
+                multi-line
+                v-model="editedDescription"
+              />
+            </v-card-text>
+          </v-flex>
+        </v-layout>
+        <v-divider></v-divider>
+        <v-layout row>
+          <v-flex xs12>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn text @click="onCancel">Cancel</v-btn>
+              <v-btn class="success" text @click="onSave">Save</v-btn>
+            </v-card-actions>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  props: ["ad"],
+  data() {
+    return {
+      modal: false,
+      editedDescription: this.ad.description,
+      editedTitle: this.ad.title
+    };
+  },
+  methods: {
+    onCancel() {
+      this.editedDescription = this.ad.description;
+      this.editedTitle = this.ad.title;
+      this.modal = false;
+    },
+    onSave() {
+      if (this.editedDescription !== "" && this.editedTitle !== "") {
+        this.$store.dispatch("updateAd", {
+          title: this.editedTitle,
+          description: this.editedDescription,
+          id: this.ad.id
+        });
+
+        this.modal = false;
+      }
+    }
+  }
+};
+</script>
